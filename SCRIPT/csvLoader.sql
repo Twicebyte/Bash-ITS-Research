ca04f96987a8080dfa254fa3fac7f5e49e326f95
Tretyakov Inniokenty <kent@vplicei.net>
2017-12-19 06:51:51 +0300

    Ufa Intercity Routes and CsvLoader uploaded

---
+++ SCRIPT/csvLoader.sql
@@ -0,0 +1,10 @@
+﻿--truncate table ReestrLocations
+
+BULK INSERT Ufa_Intercity
+    FROM N'C:\Users\twice\Documents\GitHub\Bash-ITS-Research\INPUT\Ufa_Intercity_Routes.csv'
+    WITH
+    (
+	FIRSTROW = 2,
+    FIELDTERMINATOR = ';',  --CSV field delimiter
+    ROWTERMINATOR = '\n'   --Use to shift the control to next row
+    )
